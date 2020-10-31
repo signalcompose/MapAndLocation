dae9bf99bdc9e5359ad8bcb2ee0ac87f6593bcc1
hiroshi yamato <yamato@gmail.com>
2020-10-31 20:13:09 +0900

    Initial Commit

---
+++ MapAndLocation/MapAndLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAndLocationApp.swift
+//  MapAndLocation
+//
+//  Created by hiroshi yamato on 2020/10/31.
+//
+
+import SwiftUI
+
+@main
+struct MapAndLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
